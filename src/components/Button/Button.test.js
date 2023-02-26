ee9ed7ab4467a199b37b2a8f371869aee8b74708
Carlos E Alford <webshuriken@gmail.com>
2023-02-26 09:04:52 +0000

    test: add UI test for Button.js component

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 import Button from './Button.js';
 
 
-test('renders a button with a value other than Text', () => {
+test('renders a button with the value Test', () => {
   render(<Button bText="Test" />);
   const button = screen.getByRole('button');
   expect(button).toHaveTextContent('Test');
