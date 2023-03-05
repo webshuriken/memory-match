5ad1d4f4feb0ef002dcd6cbcc6c01849108b3af9
Carlos E Alford <10241920+webshuriken@users.noreply.github.com>
2023-03-05 19:22:41 +0000

    feat: SidePanelTimer component now ticks forever

--- src/components/SidePanelTimer/SidePanelTimer.js
+++ src/components/SidePanelTimer/SidePanelTimer.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 
 function SidePanelTimer() {
+  // array represents seconds, minutes
+  const [mins, setMins] = useState(0);
+  const [secs, setSecs] = useState(0);
+
+  // tictoc
+  const updateTimer = () => setSecs(secs => secs + 1);
+
+  // every 60 seconds, reset the seconds and increase the minutes. Max 20mins.
+  if (secs === 60) {
+    setMins(mins => mins + 1);
+    setSecs(secs => 0);
+  }
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateTimer();
+    }, 1000);
+  
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <div>
       <p>Timer</p>
-      <p>00:00</p>
+      <p>{`${mins}`.padStart(2, '0')}{`:`}{`${secs}`.padStart(2, '0')}</p>
     </div>
   )
 }
