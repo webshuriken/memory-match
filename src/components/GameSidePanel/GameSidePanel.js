9a3313964e75c578608a7016fa8db46ba4ac5bd7
Carlos E Alford <10241920+webshuriken@users.noreply.github.com>
2023-03-11 19:44:26 +0000

    GameSidePanel component passes the new test for the move section

--- src/components/GameSidePanel/GameSidePanel.js
+++ src/components/GameSidePanel/GameSidePanel.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import SidePanelTimer from '../SidePanelTimer/SidePanelTimer.js';
 import SidePanelMoves from '../SidePanelMoves/SidePanelMoves.js';
 import GameButton from '../GameButton/GameButton.js'
 
 
 function GameSidePanel() {
+  console.log("LOADING GAME SIDE PANEL");
+  // TODO: see if there is a better place for this state
+  const [timerState, setTimerState] = useState('paused');
+
+  function resetGame() {
+    console.log("LETS RESET THE GAME");
+  }
+  
+  function quitGame() {
+    console.log("LETS QUIT THE GAME");
+  }
+
   return (
     <article>
-      <SidePanelTimer />
-      <SidePanelMoves />
-      <GameButton />
-      <GameButton />
+      <SidePanelTimer state={timerState} />
+      <div>
+        <p>Moves</p>
+        <p>0</p>
+      </div>
+      <GameButton text="Reset" cb={resetGame} />
+      <GameButton text="Quit" cb={quitGame} />
     </article>
   )
 }
