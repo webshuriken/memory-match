9d8b8ece1e6fe3f6a301dc472a67b28e82a2d315
Web Shuriken <webshuriken@gmail.com>
2024-01-18 19:05:00 +0000

    Add a unit test to the Button component
    Test the textContent

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,15 +1,18 @@
 import renderer from 'react-test-renderer';
-import { describe, expect, test } from '@jest/globals';
+import { describe, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
 import Button from './Button';
 
+// snapshot tests
 it('renders a button correctly', () => {
   const component = renderer.create(<Button value="Play" />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 })
 
-// test('renders a button with the value Test', () => {
-//   render(<Button value="Test" />);
-//   const button = screen.getByRole('button');
-//   expect(button).toHaveTextContent('Test');
-// });
+// Units tests
+test('renders a button with the value Play', () => {
+  render(<Button value="Play" />);
+  const button = screen.getByRole('button');
+  expect(button).toHaveTextContent('Play');
+});
