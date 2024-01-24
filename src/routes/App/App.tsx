395da476e9595b791eca3d98ac5fe641a4a88631
Web Shuriken <webshuriken@gmail.com>
2024-01-24 07:49:09 +0000

    App component typescript ready

--- src/routes/App/App.tsx
+++ src/routes/App/App.tsx
@@ -3,26 +3,18 @@ import './App.css';
 import Nav from '../../components/Nav/Nav';
 
 
-// TODO:
-// - Implemente react router
-// - I dont know if i want to separate the Header and Footer into their own components..
-// - Import and use the Nav component
 export default function App(): JSX.Element {
-  console.log("APP component reloading: ", Outlet)
 
   return (
-    <section className="App">
-      <header>
-        <h2>Memory Match</h2>
+    <div className="App">
+      <header className="app-header">
+        <span>Memory Match</span>
         <Nav />
       </header>
+      {/* each page declares its own main element */}
       <main>
         <Outlet />
       </main>
-      <footer>
-        <p data-testid="footerCopyright">2023 Memory Match - All rights reserved.</p>
-        <p>Developed by <a href="https://carlosealford.com">Carlos E Alford</a></p>
-      </footer>
-    </section>
+    </div>
   );
 }
