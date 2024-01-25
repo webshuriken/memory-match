a2d6852dd006ae776dd3fa50281649d976210e09
Web Shuriken <webshuriken@gmail.com>
2024-01-25 21:30:57 +0000

    update Nav component to be typescript friendly

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,23 +1,35 @@
 import { Link } from "react-router-dom"
+import { ChangeEvent, MouseEventHandler, useState } from "react"
 
 
 export default function Nav(): JSX.Element {
+  const [styles, setStyles] = useState({display: 'none'});
+
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    setStyles({display: styles.display === 'none' ? 'block' : 'none'})
+  }
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to={'/'}>Play</Link>
-        </li>
-        <li>
-          <Link to={'leaderboard'}>Leaderboard</Link>
-        </li>
-        <li>
-          <Link to={'settings'}>Settings</Link>
-        </li>
-        <li>
-          <Link to={'about'}>About</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <button onClick={handleClick}>
+        <label htmlFor="menu-button">Menu</label>
+      </button>
+      <input type="checkbox" id="menu-button" />
+      <nav style={styles}>
+        <ul>
+          <li>
+            <Link to={'/'}>Play</Link>
+          </li>
+          <li>
+            <Link to={'leaderboard'}>Leaderboard</Link>
+          </li>
+          <li>
+            <Link to={'settings'}>Settings</Link>
+          </li>
+          <li>
+            <Link to={'about'}>About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
