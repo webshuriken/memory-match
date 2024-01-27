15d66318af8631161891389c2dd69acbfaafd9eb
Web Shuriken <webshuriken@gmail.com>
2024-01-27 10:43:19 +0000

    refactor: remove no longer needed modules from Nav component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { ChangeEvent, MouseEventHandler, useState } from "react"
+import { useState } from "react"
 
 
 export default function Nav(): JSX.Element {
