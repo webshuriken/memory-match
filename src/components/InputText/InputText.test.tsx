95e38610562625c2d6a45693f2a405343dba4c96
Web Shuriken <webshuriken@gmail.com>
2024-01-27 22:13:52 +0000

    test: Update the InputText component tests

---
+++ src/components/InputText/InputText.test.tsx
@@ -0,0 +1,50 @@
+import { act, screen, render, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import InputText from "./InputText";
+import { HTMLInputTypeAttribute } from "react";
+
+
+describe('renders the InputText component', () => {
+
+  let theLabel: HTMLElement;
+
+  beforeEach(() => {
+    render(<InputText />);
+    theLabel = screen.getByRole('textbox').parentElement as HTMLElement;
+  });
+
+  test('renders the input element', () => {
+    expect(theLabel).toBeInTheDocument();
+  });
+
+  test('label text is correct', () => {
+    const textExpected = 'enter your name';
+    expect(theLabel).toHaveTextContent(textExpected);
+  });
+
+  test('render the input element', () => {
+    const input: HTMLInputElement = within(theLabel).getByRole('textbox');
+    expect(input).toBeInTheDocument();
+  });
+
+  test('the input element has a start value of empty string', () => {
+    expect(within(theLabel).getByRole('textbox')).toHaveTextContent('');
+  });
+
+  test('the input placeholder has correct text', () => {
+    const placeholderText = 'enter your name';
+    expect(within(theLabel).getByPlaceholderText(placeholderText));
+  });
+
+  test('the input value updates when user types in', () => {
+    const input: HTMLInputElement = within(theLabel).getByRole('textbox');
+    const userText = 'Elliott Dorian'
+
+    act(() => {
+      userEvent.type(input, userText);
+    });
+
+    expect(input.value).toMatch(userText);
+  });
+
+});
