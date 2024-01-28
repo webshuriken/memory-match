2ddc18421cc5c4880c2a63e3194d86927528451c
Web Shuriken <webshuriken@gmail.com>
2024-01-28 23:10:54 +0000

    feat: Add props type to Moves component

--- src/components/Moves/Moves.tsx
+++ src/components/Moves/Moves.tsx
@@ -1,4 +1,8 @@
-export default function Moves({ children }: {children: string}): JSX.Element {
+type Props = {
+  children: string
+}
+
+export default function Moves({ children }: Props): JSX.Element {
   return (
     <article>
       <p>Moves</p>
