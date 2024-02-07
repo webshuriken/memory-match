034bde115116e2f6db6f7b844d9615832b335a97
Web Shuriken <webshuriken@gmail.com>
2024-02-07 20:37:10 +0000

    feat: Turn InputText into a controlled component
    Add its own state, and now takes in a function from parent to pass in the players name to use by app

--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -1,16 +1,29 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState } from 'react';
 
 
 type Props = {
-  handleInput: (value: ChangeEvent<HTMLInputElement>) => void;
-  value: string;
+  handlePlayerName: (value: string) => void;
 }
 
-export default function InputText({ handleInput, value }: Props): JSX.Element {
+export default function InputText({ handlePlayerName }: Props): JSX.Element {
+  const [value, setValue] = useState<string>('');
+
+  function handleInput(e: ChangeEvent<HTMLInputElement>):void {
+    setValue(e.target.value);
+    handlePlayerName(e.target.value);
+  }
+
   return (
     <label>
       <span>enter your name</span>
-      <input placeholder="max 20 characters" type="text" name="name" value={value} onChange={handleInput} />
+      <input 
+        placeholder="max 20 characters" 
+        type="text" 
+        name="name" 
+        maxLength={20}
+        value={value} 
+        onChange={handleInput}
+      />
     </label>
   )
 }
