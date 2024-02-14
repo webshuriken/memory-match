23c330dbf86bee694df0d492134ed98f6bc174a0
Web Shuriken <webshuriken@gmail.com>
2024-02-14 10:12:32 +0000

    chore: remove comments with no use

--- src/routes/App/App.tsx
+++ src/routes/App/App.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import './App.css';
 import Nav from '../../components/Nav/Nav';
 
 
@@ -11,7 +10,6 @@ export default function App(): JSX.Element {
         <span>Memory Match</span>
         <Nav />
       </header>
-      {/* each page declares its own main element */}
       <main>
         <Outlet />
       </main>
