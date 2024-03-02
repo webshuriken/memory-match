9cb2203f6348608a398a349feba45a97e2c6283d
Web Shuriken <webshuriken@gmail.com>
2024-03-02 19:38:09 +0000

    feat: implement react-router built-in context to pass game settings

--- src/routes/App/App.tsx
+++ src/routes/App/App.tsx
@@ -1,8 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Outlet, useOutletContext } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
+import { DeckOfCards } from '../../globals/gameData';
+import { InitLeaderboard } from '../../globals/gameData';
+import { iGameContextType } from '../../globals/types';
 
 
 export default function App(): JSX.Element {
+  const [theGame, setTheGame] = useState<iGameContextType | null>();
+
+  useEffect(() => {
+    const state:iGameContextType = {
+      game: {
+        deckOfCards: DeckOfCards,
+        player: {
+          moves: '',
+          name: '',
+          time: ''
+        }
+      },
+      leaderboard: InitLeaderboard
+    }
+    // prep the game
+    setTheGame(state);
+  }, []);
+
+  console.log("APP SETTINGS: ", theGame);
 
   return (
     <div className="App">
@@ -11,8 +34,20 @@ export default function App(): JSX.Element {
         <Nav />
       </header>
       <main>
-        <Outlet />
+        <Outlet context={theGame} />
       </main>
     </div>
   );
+}
+
+// active game deck and player stats
+export function useGame() {
+  const gameContext = useOutletContext<iGameContextType>();
+  return gameContext.game;
+}
+
+// Leaderboard context
+export function useLeaderboard() {
+  const gameContext = useOutletContext<iGameContextType>();
+  return gameContext.leaderboard;
 }
