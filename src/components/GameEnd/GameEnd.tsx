c1077f28148b2ad91dc8fbb3864a68a7937da669
Web Shuriken <webshuriken@gmail.com>
2024-03-10 08:12:00 +0000

    feat: GameEnd component checks for profanities and short string lengths

--- src/components/GameEnd/GameEnd.tsx
+++ src/components/GameEnd/GameEnd.tsx
@@ -1,8 +1,4 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import InputText from "../InputText/InputText";
-import ButtonChip from "../ButtonChip/ButtonChip";
-import ErrorMessage from "./ErrorMessage";
 // moves, timer context
 import { useMovesContext } from "../../context/MovesContext";
 import { useTimerContext } from "../../context/TimerContext";
@@ -11,74 +7,48 @@ const BadWordsArray = require('badwords/array');
 const Filter = require('badwords-filter');
 
 
-interface PlayerName {
-  name: string;
-  isProfanity: boolean;
-  isTooShort: boolean;
-}
-
-interface PlayerStats {
-  name: string;
-  time: {
-    mins: number;
-    secs: number;
-  },
-  moves: number;
+type NameError = {
+  short?: string;
+  profanity?: string;
+  continue: boolean;
 }
 
 export default function GameEnd(): JSX.Element {
-  const [player, setPlayer] = useState<PlayerName>({
-    name: '',
-    isProfanity: false,
-    isTooShort: false
-  });
-  // this is so we can manually navigate to the desired page
-  const navigate = useNavigate();
-  // context
-  const moves = useMovesContext();
-  const { ticking, minutes, seconds } = useTimerContext();
+  // lets control this Form component
+  const [playerName, setPlayerName] = useState('');
+  const [nameError, setNameError] = useState<NameError>({ continue: false })
 
-  /**
-   * Simply updates the this components state with user text
-   * @param {string} name - the text entered by the user
-   */
-  function handlePlayerName(name: string): void {
-    setPlayer(prevState => ({ ...prevState, name: name }));
+  // lets get some context
+  const gameMoves = useMovesContext();
+  const gameTimer = useTimerContext();
+
+  function handlePlayerName(e: React.ChangeEvent<HTMLInputElement>) {
+    setPlayerName(e.target.value);
   }
 
-  /**
-   * Checks the text entered by user and presents error messages
-   * @returns undefined - used to exit early
-   */
-  function handleClick(event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLButtonElement>): void {
-    // in case someone tries to enter an empty string
-    if (player.name.length === 0) return;
-
-    // create filter of badwords
-    const filter = new Filter({ list: BadWordsArray, useRegex: true });
-    // check minimum length and if there are any profanities
-    const tooShort = player.name.length < 4 ? true : false;
-    const profanity = filter.isUnclean(player.name);
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const error: NameError = { continue: false };
     
-    // update state if checks are not met, this is so we rerender page or lets moves on to leaderboard
-    if (tooShort || profanity) {
-      // update the state with results
-      setPlayer(prevState => ({ ...prevState, isProfanity: profanity, isTooShort: tooShort }));
-    }else{
-      prepPlayerStats();
+    // is the word long enough
+    if (playerName.length < 4) {
+      error.short = 'Your name has to be between 4 and 20 characters';
     }
-  }
+    
+    // create bad words filter
+    const filter = new Filter({ list: BadWordsArray, useRegex: true });
+    if (filter.isUnclean(playerName)) {
+      error.profanity = 'What did we say about bad words..';
+    }
 
-  function prepPlayerStats(): void {
-    const latestPlayerStats: PlayerStats = {
-      name: player.name,
-      time: {
-        mins: minutes,
-        secs: seconds
-      },
-      moves: moves
+    if (Object.keys(error).length > 0) {
+      setNameError(error);
     }
-    console.log("HERE IS THE GAME STATS: ", latestPlayerStats)
+
+    // if we made it thus far then we good
+    error.continue = true;
+
+    // TODO: PREPP THE DATA FOR THE LEADERBOARD
   }
 
   return (
@@ -87,9 +57,33 @@ export default function GameEnd(): JSX.Element {
         <h2>Congratulations</h2>
       </header>
       <div>
-        <InputText handlePlayerName={handlePlayerName} />
-        <ErrorMessage isProfanity={player.isProfanity} isTooShort={player.isTooShort} />
-        <ButtonChip handleClick={handleClick} value='Continue' />
+        <form onSubmit={handleSubmit}>
+          <p>
+            <label>
+              <span>enter your name</span>
+              <input 
+                placeholder="max 20 characters" 
+                type="text" 
+                name="player_name" 
+                maxLength={20}
+                onChange={handlePlayerName}
+              />
+              {
+                !nameError.continue 
+                ?
+                  <></>
+                :
+                  <span>
+                    <span>{nameError.short}</span>
+                    <span>{nameError.profanity}</span>
+                  </span>
+              }
+            </label>
+          </p>
+          <p>
+            <button type="submit">Continue</button>
+          </p>
+        </form>
       </div>
     </article>
   )
