ef1941c99b92599e2affa798656c7dab2be1585c
Web Shuriken <webshuriken@gmail.com>
2024-09-16 19:35:46 +0100

    style: improves navbar transition when opened and closed

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,20 +2,31 @@ import { NavLink } from "react-router-dom"
 import { useState } from "react"
 import './Nav.css';
 
+type MenuStateType = {
+  open: boolean;
+  class: string;
+}
 
 export default function Nav(): JSX.Element {
-  const [styles, setStyles] = useState({display: 'none'});
+  const [menu, setMenu] = useState<MenuStateType>({ open: false, class: 'menu' });
 
   function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    setStyles({display: styles.display === 'none' ? 'block' : 'none'})
+    // update state
+    setMenu(state => {
+      return {
+        open: !state.open,
+        class: state.open ? 'menu open' : 'menu',
+      }
+    });
   }
+  console.log("RUNNS ON EACH PAGE LOAD? ");
   return (
     <>
       <button className="menu-button button-chip button-chip__yellow button__shine-effect" onClick={handleClick} aria-haspopup={'menu'} aria-expanded={false}>
         <label htmlFor="menuButtonCheckbox">Menu</label>
       </button>
       <input type="checkbox" id="menuButtonCheckbox" />
-      <nav style={styles} className="menu">
+      <nav className={menu.class}>
         <ul className="menu-list">
           <li>
             <NavLink className="menu-list__link button__shine-effect" to={'/'}>Play</NavLink>
