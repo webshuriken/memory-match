43591f9a4ae4fbb387d49f186bbe1540940ec689
Web Shuriken <webshuriken@gmail.com>
2024-09-19 22:38:06 +0100

    feat: GameCards logic for flipping cards is complete

--- src/components/GameCards/GameCards.tsx
+++ src/components/GameCards/GameCards.tsx
@@ -20,27 +20,6 @@ export default function GameCards({ handleCardClick }: Props): JSX.Element {
   const [flippedCards, setFlippedCards] = useState<FlippedCardsType>([]);
   // we just need the face cards and the cover
   const [deckOfCards, setDeckOfCards] = useState<iCardsType | null>(null);
-  console.log("DECK: ", deckOfCards)
-
-  /*
-   * TODO: LOGIC FOR THE CARDS FLIPPIN
-   * ===========
-   * use a function to update the deck of cards to flip cards when required
-   * 1. will flip the card if lastcardflip === undefine
-   * 2. if lastcardflip set then check that this card is not the one already flipped and flip it, otherwise do nothing
-   * 3. allow for multiple cards to be flipped back (like a reset if there is no match)
-   * ===========
-   * 1. if lastcardflip === undefined, 
-   *  - update lastcardflip to this id and its pairID
-   *  - increment moves
-   * 2. if current flip id is not lastcardflip id then
-   *  - increment moves
-   *  - check if the cards are pair
-   *   - if the cards are not pairs, reset the cards by flipping them back
-   *   - if they are pairs then matchFound = true and dont flip the cards
-   *    - MAKE SURE TO DISABLE THEIR CLICK ABILITIES
-   */
-
 
   /**
    * Takes the name of an image and returns the URL made available by Cloudinary
@@ -56,25 +35,28 @@ export default function GameCards({ handleCardClick }: Props): JSX.Element {
 
   /**
    * 
-   * @param reveal boolean - whether to reveal the card or hide it
-   * @param card <iCardFacesType> - takes in a card to flip
+   * @param cards <FlippedCardType[]> - an array of 1 card or 2 cards that have been flipped
+   * @param stayFlipped boolean - lets us know if we should flip the card
    */
   function updateCardsFace(cards: FlippedCardsType, stayFlipped: boolean): void {
-
     // update the deckOfCards
     if (deckOfCards !== null) {
       let faces: iCardFacesType[];
+      // only flip a single card
       if (cards.length === 1) {
         const [firstCard] = cards;
         faces = deckOfCards.faces.map(face => (face.id === firstCard.id) ? { ...face, flipped: stayFlipped } : face);
       }else{
+        // we are flippin two card back or keeping them wide open
         const [firstCard, secondCard] = cards;
         faces = deckOfCards.faces.map(face => (face.id === firstCard.id || face.id === secondCard.id) ? { ...face, flipped: stayFlipped } : face);
       }
+      // locally mutate a new deck
       const deck = {
         ...deckOfCards,
         faces: faces
       };
+      // now update the state with local mutation
       setDeckOfCards(deck);
       // reset the flipped cards state
       if (cards.length === 2) {
@@ -93,28 +75,46 @@ export default function GameCards({ handleCardClick }: Props): JSX.Element {
       return;
     }
     
+    // here for TS sakes
     if (deckOfCards !== null) {
       // update the deckOfCards
       updateCardsFace([{id, pairID}], true);
     }
 
-    const newFlippedCards = [...flippedCards, { id, pairID }];
+    let matchFound = false;
+    let bustaMove = false;
+
+    // locally mutate for further use and state update
+    let newFlippedCards: FlippedCardsType;
+    if (flippedCards.length === 0 || flippedCards.length === 1 && flippedCards[0].id !== id) {
+      console.log("NO REPEATS")
+      newFlippedCards = [...flippedCards, { id, pairID }];
+      bustaMove = true;
+    }else{
+      newFlippedCards = flippedCards;
+    }
     setFlippedCards(newFlippedCards);
 
+    // with two cards facing  us we can check if they are a match
     if (newFlippedCards.length === 2) {
-      checkForMatch(newFlippedCards);
+      console.log("WILL A SECOND CLICK OF THE SAME CARD")
+      // are they matching
+      if (newFlippedCards[0].pairID === newFlippedCards[1].id) {
+        matchFound = true;
+        updateCardsFace(newFlippedCards, true);
+      }else{
+        setTimeout(() => {
+          updateCardsFace(newFlippedCards, false);
+        }, 600);
+      }
     }
 
-    // TODO: games need to be able to reset
-    // TODO: activate timer
-    // TODO: count moves
-    // TODO: parent to deal with other updates
-    // handleCardClick(matchFound, incrementMoves);
+    handleCardClick(matchFound, bustaMove);
   }
 
   function checkForMatch(flippedCards: FlippedCardsType): void {
     let matchFound = false;
-
+    
     if (flippedCards[0].pairID === flippedCards[1].id) {
       matchFound = true;
       updateCardsFace(flippedCards, true);
@@ -124,7 +124,6 @@ export default function GameCards({ handleCardClick }: Props): JSX.Element {
       }, 600);
     }
   }
-
 
   /**
    * Add meta to each card to play the game
