fed39421a559e62d89a00ea3147dd30f60761191
Web Shuriken <webshuriken@gmail.com>
2024-09-30 16:35:20 +0100

    chore: improve Nav.tsx logic so menu auto closes on page navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,10 +7,14 @@ type MenuStateType = {
   class: string;
 }
 
+// function is used by anchor and button elements to open menu
+type HandleClickType = React.MouseEvent<HTMLButtonElement, MouseEvent> | React.MouseEvent<HTMLAnchorElement, MouseEvent>;
+
+
 export default function Nav(): JSX.Element {
   const [menu, setMenu] = useState<MenuStateType>({ open: false, class: 'menu' });
 
-  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  function handleClick(event: HandleClickType) {
     // update state
     setMenu(state => {
       return {
@@ -29,16 +33,16 @@ export default function Nav(): JSX.Element {
       <nav className={menu.class}>
         <ul className="menu-list">
           <li>
-            <NavLink className="menu-list__link button__shine-effect" to={'/'}>Play</NavLink>
+            <NavLink className="menu-list__link button__shine-effect" to={'/'} onClick={handleClick}>Play</NavLink>
           </li>
           <li>
-            <NavLink className="menu-list__link button__shine-effect" to={'leaderboard'}>Leaderboard</NavLink>
+            <NavLink className="menu-list__link button__shine-effect" to={'leaderboard'} onClick={handleClick}>Leaderboard</NavLink>
           </li>
           <li>
-            <NavLink className="menu-list__link button__shine-effect" to={'settings'}>Settings</NavLink>
+            <NavLink className="menu-list__link button__shine-effect" to={'settings'} onClick={handleClick}>Settings</NavLink>
           </li>
           <li>
-            <NavLink className="menu-list__link button__shine-effect" to={'about'}>About</NavLink>
+            <NavLink className="menu-list__link button__shine-effect" to={'about'} onClick={handleClick}>About</NavLink>
           </li>
         </ul>
       </nav>
