f0a037771bcba58b6e6ec4baf9a5af0d6241f2b6
Web Shuriken <webshuriken@gmail.com>
2024-10-02 16:47:35 +0100

    refactor: Settings component to add keys to each list item

--- src/routes/Settings/Settings.tsx
+++ src/routes/Settings/Settings.tsx
@@ -7,8 +7,11 @@ import './Settings.css';
 export default function Settings(): JSX.Element {
   // global context so we can change some game settings
   const [settings, setSettings] = useSettings();
-
   const msg = "Want to make some changes to the game?";
+
+  function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+    console.log("WHEN YOU ARE NOT HERE WITH ME");
+  }
 
   return (
     <section className="settings">
@@ -26,18 +29,18 @@ export default function Settings(): JSX.Element {
               settings.availableDecks.map((deck, index) => {
                 const buttonState = settings.activeDeckIndex === index ? "active" : "disabled";
                 return (
-                  <li>
+                  <li key={index}>
                     <article className="deck-view">
                       <img src={fetchImageURL(deck.cards.cover.src)} alt="" className="deck-view__cover" />
                       <h3 className="deck-view__name">{deck.theme}</h3>
                       <p className="deck-view__authors">{deck.author.name.join(", ")}</p>
-                      <button className={`deck-view__button ${buttonState}`}>
+                      <button onClick={handleClick} className={`deck-view__button ${buttonState}`}>
                         {buttonState}
                       </button>
                       <ul className="deck-view__face-list">
                         {
                           deck.cards.faces.map(face => (
-                            <li>
+                            <li key={face.src}>
                               <img src={fetchImageURL(face.src)} alt={deck.cards.alt} className="deck-view__face-list-img" />
                             </li>
                           ))
