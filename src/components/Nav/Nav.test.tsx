a00d0b1fbb1a89647c1f00fb50290c3aee9d9d35
webshuriken <webshuriken@gmail.com>
2024-10-09 17:41:50 +0100

    refactor: Refactor Nav component tests and update menu button behavior

--- src/components/Nav/Nav.test.tsx
+++ src/components/Nav/Nav.test.tsx
@@ -1,91 +1,126 @@
-import { act, render, screen, within } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import Nav from "./Nav";
 
-let component: any;
 
-beforeEach(() => {
-  component = render(
-    <MemoryRouter>
-      <Nav />
-    </MemoryRouter>
-  );
-});
+describe('Navigation component', () => {
+  describe('renders the menu button', () => {
+    test('with correct name', () => {
+      render(<Nav />, { wrapper: MemoryRouter });
+      const menuButton = screen.getByRole('button', {name: /menu/i});
+      expect(menuButton).toBeInTheDocument();
+    });
+    
+    test('with menu text wrapped in a label', () => {
+      render(<Nav />, { wrapper: MemoryRouter });
+      expect(screen.getByLabelText(/menu/i)).toBeTruthy();
+    });
+  
+    test('click on menu button, semantically reveals the nav list', () => {
+      render(<Nav />, { wrapper: MemoryRouter });
 
-describe('renders the menu button', () => {
-  test('with correct name', () => {
-    const menuButton = screen.getByRole('button', {name: /menu/i});
-    expect(menuButton).toBeInTheDocument();
+      act(() => {
+        userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      });
+
+      expect(screen.getByRole('navigation')).toBeInTheDocument();
+    });
   });
   
-  test('with menu text wrapped in a label', () => {
-    expect(screen.getByLabelText(/menu/i)).toBeTruthy();
-  });
+  describe('The navigation list', () => {
+    test('renders as a list', () => {
+      render(<Nav />, { wrapper: MemoryRouter });
 
-  test('click on menu button, semantically reveals the nav list', () => {
-    act(() => {
-      userEvent.click(screen.getByRole('button', {name: /menu/i}));
-    });
-    expect(screen.getByRole('navigation')).toBeInTheDocument();
-  });
-});
+      act(() => {
+        // dont act out because of the state
+        userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      });
+      const appNav = screen.getByRole('navigation');
 
-describe('The navigation list', () => {
-  let appNav: HTMLElement;
-
-  beforeEach(() => {
-    act(() => {
-      // dont act out because of the state
-      userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      expect(within(appNav).getByRole('list')).toBeInTheDocument();
     });
 
-    // screen now visible
-    appNav = screen.getByRole('navigation');
-  });
+    test('has 4 items', () => {
+      render(<Nav />, { wrapper: MemoryRouter });
 
-  test('renders as a list', () => {
-    expect(within(appNav).getByRole('list')).toBeInTheDocument();
-  });
+      act(() => {
+        // dont act out because of the state
+        userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      });
+      const appNav = screen.getByRole('navigation');
 
-  test('has 4 items', () => {
-    expect(within(appNav).getByRole('list').children).toHaveLength(4);
-  });
+      expect(within(appNav).getByRole('list').children).toHaveLength(4);
+    });
 
-  test('has root link with correct href and name', () => {
-    const expectedLinkName = /play/i;
-    const expectedLinkURL = "/";
-    const homeLink = within(appNav).getAllByRole('link')[0];
+    test('has root link with correct href and name', () => {
+      const expectedLinkName = /play/i;
+      const expectedLinkURL = "/";
+      
+      render(<Nav />, { wrapper: MemoryRouter });
+      
+      act(() => {
+        // dont act out because of the state
+        userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      });
+      const appNav = screen.getByRole('navigation');
+      const homeLink = within(appNav).getAllByRole('link')[0];
 
-    expect(homeLink).toHaveAttribute('href', expectedLinkURL);
-    expect(homeLink).toHaveTextContent(expectedLinkName);
-  });
+      expect(homeLink).toHaveAttribute('href', expectedLinkURL);
+      expect(homeLink).toHaveTextContent(expectedLinkName);
+    });
+    
+    test('has leaderboard link with correct href and name', () => {
+      const expectedLinkName = /leaderboard/i;
+      const expectedLinkURL = "/leaderboard";
+      
+      render(<Nav />, { wrapper: MemoryRouter });
+      
+      act(() => {
+        // dont act out because of the state
+        userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      });
+      const appNav = screen.getByRole('navigation');
+      const homeLink = within(appNav).getAllByRole('link')[1];
+      
+      expect(homeLink).toHaveAttribute('href', expectedLinkURL);
+      expect(homeLink).toHaveTextContent(expectedLinkName);
+    });
 
-  test('has leaderboard link with correct href and name', () => {
-    const expectedLinkName = /leaderboard/i;
-    const expectedLinkURL = "/leaderboard";
-    const homeLink = within(appNav).getAllByRole('link')[1];
+    test('has settings link with correct href and name', () => {
+      const expectedLinkName = /settings/i;
+      const expectedLinkURL = "/settings";
+      
+      render(<Nav />, { wrapper: MemoryRouter });
+      
+      act(() => {
+        // dont act out because of the state
+        userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      });
+      const appNav = screen.getByRole('navigation');
+      const homeLink = within(appNav).getAllByRole('link')[2];
+    
+      expect(homeLink).toHaveAttribute('href', expectedLinkURL);
+      expect(homeLink).toHaveTextContent(expectedLinkName);
+    });
 
-    expect(homeLink).toHaveAttribute('href', expectedLinkURL);
-    expect(homeLink).toHaveTextContent(expectedLinkName);
-  });
+    test('has about link with correct href and name', () => {
+      const expectedLinkName = /about/i;
+      const expectedLinkURL = "/about";
+      
+      render(<Nav />, { wrapper: MemoryRouter });
+      
+      act(() => {
+        // dont act out because of the state
+        userEvent.click(screen.getByRole('button', {name: /menu/i}));
+      });
+      const appNav = screen.getByRole('navigation');
+      const homeLink = within(appNav).getAllByRole('link')[3];
 
-  test('has settings link with correct href and name', () => {
-    const expectedLinkName = /settings/i;
-    const expectedLinkURL = "/settings";
-    const homeLink = within(appNav).getAllByRole('link')[2];
-
-    expect(homeLink).toHaveAttribute('href', expectedLinkURL);
-    expect(homeLink).toHaveTextContent(expectedLinkName);
-  });
-
-  test('has about link with correct href and name', () => {
-    const expectedLinkName = /about/i;
-    const expectedLinkURL = "/about";
-    const homeLink = within(appNav).getAllByRole('link')[3];
-
-    expect(homeLink).toHaveAttribute('href', expectedLinkURL);
-    expect(homeLink).toHaveTextContent(expectedLinkName);
+      expect(homeLink).toHaveAttribute('href', expectedLinkURL);
+      expect(homeLink).toHaveTextContent(expectedLinkName);
+    });
   });
 });
 
