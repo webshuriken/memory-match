86965a0e8ac1cb895fb4c3c11503a8b8a8bf2384
webshuriken <webshuriken@gmail.com>
2024-10-09 10:43:00 +0100

    test: Add tests for GameDash component

--- src/components/GameDash/GameDash.test.tsx
+++ src/components/GameDash/GameDash.test.tsx
@@ -1,35 +1,71 @@
-import { act, render, within, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import GameDash from "./GameDash";
+import { TimerProvider } from "../../context/TimerContext";
+import { MovesProvider } from "../../context/MovesContext";
 
 
 describe('GameDash component', () => {
   test('component renders', () => {
-    render(<GameDash setResetGame={jest.fn} />);
+    render(
+      <TimerProvider>
+        <MovesProvider>
+          <GameDash setResetGame={jest.fn()} />
+        </MovesProvider>
+      </TimerProvider>
+    );
     expect(screen).toBeTruthy();
   });
   
-  describe('The dashboard intial render', () => {
-    beforeEach(() => {
-      render(<GameDash setResetGame={jest.fn} />);
-    });
+  test('reset button renders', () => {
+    render(
+      <TimerProvider>
+        <MovesProvider>
+          <GameDash setResetGame={jest.fn()} />
+        </MovesProvider>
+      </TimerProvider>
+    );
+    expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument();
+  });
 
-    test('reset button renders', () => {
-      expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument();
-    });
+  test('moves component renders text and moves', () => {
+    render(
+      <TimerProvider>
+        <MovesProvider>
+          <GameDash setResetGame={jest.fn()} />
+        </MovesProvider>
+      </TimerProvider>
+    );
+    // const wrapper = screen.getByText(/moves/i);
+    expect(screen.getByText(/moves/i)).toBeInTheDocument();
+    expect(screen.getAllByRole((content, element) => {
+      return element?.textContent === '0';
+    })).toBeTruthy();
+  });
   
-    test('moves component renders text and moves', () => {
-      const wrapper = screen.getByText(/moves/i).closest('article');
-      
-      if (wrapper !== null) {
-        expect(within(wrapper).getByText(/moves/i)).toBeInTheDocument();
-        expect(within(wrapper).getByText(/0/)).toBeInTheDocument();
-      }
-    });
-    
-    test('timer component renders text and timer', () => {
-      expect(screen.getByText(/timer/i)).toBeInTheDocument();
-      expect(screen.getByRole('timer')).toBeInTheDocument();
-      expect(screen.getByText('00:00')).toBeInTheDocument();
-    });
+  test('timer component renders text and timer', () => {
+    render(
+      <TimerProvider>
+        <MovesProvider>
+          <GameDash setResetGame={jest.fn()} />
+        </MovesProvider>
+      </TimerProvider>
+    );
+    expect(screen.getByText(/timer/i)).toBeInTheDocument();
+    expect(screen.getByRole('timer')).toBeInTheDocument();
+    expect(screen.getByText('00:00')).toBeInTheDocument();
+  });
+
+  test('reset button calls resetDash function', () => {
+    const mockSetResetGame = jest.fn();
+    render(
+      <TimerProvider>
+        <MovesProvider>
+          <GameDash setResetGame={mockSetResetGame} />
+        </MovesProvider>
+      </TimerProvider>
+    );
+    const resetButton = screen.getByRole('button', { name: /reset/i });
+    fireEvent.click(resetButton);
+    expect(mockSetResetGame).toHaveBeenCalledTimes(1);
   });
 });
