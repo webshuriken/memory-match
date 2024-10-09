d1c9896f68ff8ee727e822ae426b911b18b819b0
webshuriken <webshuriken@gmail.com>
2024-10-09 17:47:12 +0100

    Refactor PageHeader component tests and update rendering logic

--- src/components/PageHeader/PageHeader.test.tsx
+++ src/components/PageHeader/PageHeader.test.tsx
@@ -6,48 +6,44 @@ import PageHeader from "./PageHeader";
 describe('PageHeader component', () => {
   // SEMANTICS
   describe('Semantics testing', () => {
-    beforeEach(() => {
-      render(
-        <MemoryRouter>
-          <PageHeader />
-        </MemoryRouter>
-      );
-    });
     test('component renders', () => {
+      render(<PageHeader />, { wrapper: MemoryRouter });
       expect(screen).toBeTruthy();
     });
+
     test('h2 and button elements render', () => {
+      render(<PageHeader />, { wrapper: MemoryRouter });
       expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
       expect(screen.getByRole('button', { name: /play again/i})).toBeInTheDocument();
     });
+
     test('heading renders with default value', () => {
+      render(<PageHeader />, { wrapper: MemoryRouter });
       const title = 'PAGE HEADER';
       expect(screen.getByRole('heading', { name: title })).toBeInTheDocument();
     });
+
     // paragraph is not detected as a role. We test for the default value within it
     test('paragraph render with default value', () => {
+      render(<PageHeader />, { wrapper: MemoryRouter });
       const msg = 'Your header paragraph goes here. Whatever text you want it to be';
       expect(screen.getByText(msg)).toBeInTheDocument();
     });
   });
+
   // DYNAMIC VALUES
   describe('Dynamic values testing', () => {
     // props values
     const title = 'Two Point Hospital';
     const msg = 'Fun game I just started playing.';
 
-    beforeEach(() => {
-      render(
-        <MemoryRouter>
-          <PageHeader title={title} msg={msg} />
-        </MemoryRouter>
-      )
-    });
-
     test(`h2 header says: ${title}`, () => {
+      render(<PageHeader title={title} msg={msg} />, { wrapper: MemoryRouter });
       expect(screen.getByRole('heading', { name: title })).toBeInTheDocument();
     });
+
     test(`paragraph says: ${msg}`, () => {
+      render(<PageHeader title={title} msg={msg} />, { wrapper: MemoryRouter });
       expect(screen.getByText(msg)).toBeInTheDocument();
     });
   });
